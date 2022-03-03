e3abe8d931b758e2c1b53ddd4cb18f85141ba563
Tim Clark <tcsoft@gmail.com>
2022-03-03 17:17:58 +0000

    new tileset

---
+++ src/tilesets/DS.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="DS" tilewidth="256" tileheight="256" tilecount="6" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="256" height="256" source="../assets/fountain.png"/>
+ </tile>
+ <tile id="1">
+  <image width="226" height="32" source="../assets/WA_Demo_Logo.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="32" source="../assets/HCL_Volt.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="32" source="../assets/HCL_Domino.png"/>
+ </tile>
+ <tile id="5">
+  <image width="64" height="32" source="../assets/Connx.png"/>
+ </tile>
+ <tile id="6">
+  <image width="64" height="32" source="../assets/DX.png"/>
+ </tile>
+</tileset>
